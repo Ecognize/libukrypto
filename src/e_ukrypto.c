cfa571a71df363589eeef63dcd22a21148b05ab2
Oleksiy Protas <elfy.ua@gmail.com>
2016-08-06 02:58:54 +0300

    Streamlined and verified code, boilerplates ready

--- src/e_ukrypto.c
+++ src/e_ukrypto.c
@@ -36,23 +36,25 @@ enum algo
     GOST_34_311_95 = 0,
     DSTU_4145_2001,
     DSTU_GOST_28147,
-    DUST_ISO_IEC_2_15946_3,
+//     DUST_ISO_IEC_2_15946_3,
+    n_algos
 };
-#define n_algos sizeof(enum algo)
 
 static int NID[n_algos];
 
 /* TODO: study numerical forms better */
-static const char *algo_desc[n_algos][3] = 
+static const char *algo_desc[][3] = 
 {
     {"1.2.3.4", "GOST 34.311-95", "GOST 34.311-95 hashing function" },
     {"1.2.3.5", "DSTU 4145-2002", "DSTU 4145-2002 digital signature algorithm" },
     {"1.2.3.6", "DSTU GOST 28147", "DSTU GOST 28147 encryption algorithm" },
-    {"1.2.3.7", "DSTU ISO/IEC 15946-3", "DSTU ISO/IEC 15946-3 key exchange" },
+//     {"1.2.3.7", "DSTU ISO/IEC 15946-3", "DSTU ISO/IEC 15946-3 key exchange" },
 };
 
 /* We need these for API returns, the actual data is filled at startup */
 static int digests[1 + 1];
+static int ciphers[1 + 1];
+static int pmeths[1 + 1];
 
 /* Message digests (hash functions) */
 static int ukrypto_digests(ENGINE *e, const EVP_MD **digest, const int **nids, int nid)
@@ -61,7 +63,29 @@ static int ukrypto_digests(ENGINE *e, const EVP_MD **digest, const int **nids, i
     if (!digest)
     {
         *nids = digests;
-        return lengthof(digests);
+        return lengthof(digests) - 1;
+    }
+}
+
+/* Ciphers */
+static int ukrypto_ciphers(ENGINE *e, const EVP_CIPHER **cipher, const int **nids, int nid)
+{
+    /* NULL parameter means we were asked about supported NIDs */
+    if (!cipher)
+    {
+        *nids = ciphers;
+        return lengthof(ciphers) - 1;
+    }
+}
+
+/* Digital Signature */
+static int ukrypto_pkeymeths(ENGINE *e, EVP_PKEY_METHOD **pmeth, const int **nids, int nid)
+{
+    /* NULL parameter means we were asked about supported NIDs */
+    if (!pmeth)
+    {
+        *nids = pmeths;
+        return lengthof(pmeths) - 1;
     }
 }
 
@@ -100,9 +124,19 @@ static int ukrypto_bind(ENGINE* e, const char *id)
     digests[0] = NID[GOST_34_311_95];
     digests[1] = 0;
     
-    /* Registering digests */
+    ciphers[0] = NID[DSTU_GOST_28147];
+    ciphers[1] = 0;
+    
+    pmeths[0] = NID[DSTU_4145_2001];
+    pmeths[1] = 0;
+    
+    /* Registering stuff */
     if (!ENGINE_set_digests(e, ukrypto_digests))
         return ukrypto_error("Can not setup digests\n");
+    if (!ENGINE_set_ciphers(e, ukrypto_ciphers))
+        return ukrypto_error("Can not setup ciphers\n");
+    if (!ENGINE_set_pkey_meths(e, ukrypto_pkeymeths))
+        return ukrypto_error("Can not setup public key methods\n");
     
     return 1;
 }

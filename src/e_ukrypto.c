3f93674ff4e5eaeb364698ac8dc349fd4e65b686
Oleksiy Protas <elfy.ua@gmail.com>
2016-08-06 02:34:08 +0300

    More advanced OpenSSL startup

--- src/e_ukrypto.c
+++ src/e_ukrypto.c
@@ -20,34 +20,93 @@
 */
 #include "libukrypto.h"
 #include <openssl/engine.h>
+#include <openssl/objects.h>
+#include <stdio.h>
+#include <stdarg.h>
 
-static const char *engine_id = "ukrypto";
-static const char *engine_name = "Ukrainian cryptography standards implementation";
+/* Static array length if # of elements */
+#define lengthof(x) (sizeof(x)/sizeof(x[0]))
 
-/* Mainly adapted from OpenSSL tutorial for now */
-static int bind(ENGINE* e, const char *id)
+static const char *ukrypto_id = "ukrypto";
+static const char *ukrypto_name = "Ukrainian cryptography standards implementation";
+
+/* Supported alorithms */
+enum algo
 {
-    /* TODO: prevent loading multiple times maybe? */
-    /* TODO: rewrite this completely anyway */
-    int ret = 0;
-    
-    if (!ENGINE_set_id(e, engine_id))
+    GOST_34_311_95 = 0,
+    DSTU_4145_2001,
+    DSTU_GOST_28147,
+    DUST_ISO_IEC_2_15946_3,
+};
+#define n_algos sizeof(enum algo)
+
+static int NID[n_algos];
+
+/* TODO: study numerical forms better */
+static const char *algo_desc[n_algos][3] = 
+{
+    {"1.2.3.4", "GOST 34.311-95", "GOST 34.311-95 hashing function" },
+    {"1.2.3.5", "DSTU 4145-2002", "DSTU 4145-2002 digital signature algorithm" },
+    {"1.2.3.6", "DSTU GOST 28147", "DSTU GOST 28147 encryption algorithm" },
+    {"1.2.3.7", "DSTU ISO/IEC 15946-3", "DSTU ISO/IEC 15946-3 key exchange" },
+};
+
+/* We need these for API returns, the actual data is filled at startup */
+static int digests[1 + 1];
+
+/* Message digests (hash functions) */
+static int ukrypto_digests(ENGINE *e, const EVP_MD **digest, const int **nids, int nid)
+{
+    /* NULL parameter means we were asked about supported NIDs */
+    if (!digest)
     {
-        fprintf(stderr, "ENGINE_set_id failed\n");
-        goto end;
+        *nids = digests;
+        return lengthof(digests);
     }
-    if (!ENGINE_set_name(e, engine_name))
-    {
-        fprintf(stderr, "ENGINE_set_name failed\n");
-        goto end;
-    }
-    
-    ret = 1;
-    
-    end:
-    return ret;
 }
 
-IMPLEMENT_DYNAMIC_BIND_FN(bind)
+/* Prints an error message and returns 1 */
+static int ukrypto_error(const char *fmt, ...)
+{
+    va_list ap;
+    va_start(ap, fmt);
+    vfprintf(stderr, fmt, ap);
+    
+    return 1;
+}
+
+/* Mainly adapted from OpenSSL tutorial for now */
+static int ukrypto_bind(ENGINE* e, const char *id)
+{
+    /* TODO: test what happens on loading/unloading and multiple loads */
+    /* TODO: rewrite this completely anyway */
+    
+    /* Basic setup */
+    if (!ENGINE_set_id(e, ukrypto_id))
+        return ukrypto_error("Can not set engine id\n");
+    if (!ENGINE_set_name(e, ukrypto_name))
+        return ukrypto_error("Can not set engine name\n");
+    
+    /* Allocating NIDs for our algorithms */
+    /* TODO: study if we need to add them to OpenSSL permanently or something */
+    for (int i = 0; i < n_algos; i++)
+    {
+        /* Not totally sure if OBJ_create indicates errors, but let's try */
+        if (! ( NID[i] = OBJ_create(algo_desc[i][0], algo_desc[i][1], algo_desc[i][2]) ) )
+            return ukrypto_error("Can not create a NID for %s\n", algo_desc[i][1]);
+    }
+    
+    /* More hard-coded that I would like to, but works */
+    digests[0] = NID[GOST_34_311_95];
+    digests[1] = 0;
+    
+    /* Registering digests */
+    if (!ENGINE_set_digests(e, ukrypto_digests))
+        return ukrypto_error("Can not setup digests\n");
+    
+    return 1;
+}
+
+IMPLEMENT_DYNAMIC_BIND_FN(ukrypto_bind)
 IMPLEMENT_DYNAMIC_CHECK_FN()
 

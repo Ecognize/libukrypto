25c4bb9394428625be3580be25a005c94ae65c51
Oleksiy Protas <elfy.ua@gmail.com>
2016-12-01 19:53:05 +0100

    Docker-based builds for exotic CPUs

---
+++ .travis/cross.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# Run the build on a selected architecture
+# More info at: https://hub.docker.com/r/multiarch/debian-debootstrap/
+# https://github.com/travis-ci/travis-ci/issues/3376#issuecomment-168003756
+
+# Register binfmt handlers
+docker run --rm --privileged multiarch/qemu-user-static:register
+
+# Run the build on specified arch
+docker run -it --rm -v $(pwd):/usr/src/libukrypto multiarch/debian-debootstrap:${ARCH} /bin/bash -x /usr/src/libukrypto/.travis/build.sh
